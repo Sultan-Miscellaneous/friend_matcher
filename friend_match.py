61ee2cc8d41f8e54cd3d8db19fa7261e4a2da35e
asultan123 <asultan@aucegypt.edu>
2020-10-22 02:09:25 -0400

    userdefined processing works

--- friend_match.py
+++ friend_match.py
@@ -1,11 +1,35 @@
 import csv
 from functools import partial
+import spacy
+
+print('Loading spacey default model')
+nlp = spacy.load("en_core_web_lg")
+print('Done')
+
+def process_text(text):
+    doc = nlp(text.lower())
+    result = []
+    for token in doc:
+        if token.text in nlp.Defaults.stop_words:
+            continue
+        if token.is_punct:
+            continue
+        if token.lemma_ == '-PRON-':
+            continue
+        result.append(token.lemma_)
+    return " ".join(result)
+
+
+def calculate_similarity(text1, text2):
+    base = nlp(process_text(text1))
+    compare = nlp(process_text(text2))
+    return base.similarity(compare)
 
 
 class Student:
 
     class Attribute():
-        def __init__(self, name='', index=0, data=None, custom_rule=None):
+        def __init__(self, name='', index=0, data=None, custom_rule=None, threshold_lower=0.45, threshold_upper=0.90, comparison_method='exact'):
             self.name = name
             self.index = index
 
@@ -14,8 +38,42 @@ class Student:
             else:
                 self.value = data
 
-            self.get_attr_comparison_score = partial(
-                self.get_attr_comparison_score_with_custom_rule, custom_rule)
+            if comparison_method == 'exact':
+                self.get_attr_comparison_score = partial(
+                    self.get_attr_comparison_score_with_custom_rule, custom_rule)
+            elif comparison_method == 'spacey':
+                self.get_attr_comparison_score = partial(
+                    self.get_attr_spacey_comparison_with_thresholds, threshold_lower, threshold_upper)
+            else:
+                print("invalid comparison method selected")
+                raise
+
+        def get_attr_spacey_comparison_with_thresholds(self, threshold_lower, threshold_upper, attr2):
+            attr1 = self
+            if len(attr1.value) > len(attr2.value):
+                attr2, attr1 = attr1, attr2
+            total_score = []
+            for each_attr1_val in attr1.value:
+                scores = [0]
+                for each_attr2_val in attr2.value:
+                    similarity = calculate_similarity(
+                        each_attr1_val, each_attr2_val)
+                    print('attr1.val: ' + each_attr1_val + ', attr2_val ' + each_attr2_val + ' similarity: ' + str(similarity))
+                    if threshold_lower < similarity and similarity < threshold_upper:
+                        # don't break, maybe something more similar is coming
+                        print('similarity resolved to 0.5')
+                        scores.append(0.5)
+                    elif similarity > threshold_upper:
+                        # can't get better than this, so break
+                        print('similarity resolved to 1')
+                        scores.append(1)
+                        break
+                    else:
+                        print('similarity resolved to 0')
+                total_score.append(max(scores))
+            print(attr1.value)
+            print(total_score)
+            return sum(total_score)
 
         def get_attr_comparison_score_with_custom_rule(self, custom_rule, attr2):
             attr1 = self
@@ -57,11 +115,11 @@ class Student:
         ]
 
         self.user_defined_attributes = [
-            Student.Attribute('tv_shows', 11),
-            Student.Attribute('musical_artists', 12),
-            Student.Attribute('fav_food', 13),
-            Student.Attribute('free_time', 14),
-            Student.Attribute('dream_living', 15)
+            Student.Attribute('tv_shows', 11, threshold_lower=0.80, threshold_upper=0.95, comparison_method='spacey'),
+            Student.Attribute('musical_artists', 12, threshold_lower=0.85, threshold_upper=0.95, comparison_method='spacey'),
+            Student.Attribute('fav_food', 13, threshold_lower=0.60, threshold_upper=0.90, comparison_method='spacey'),
+            Student.Attribute('free_time', 14, threshold_lower=0.50, threshold_upper=0.90, comparison_method='spacey'),
+            Student.Attribute('dream_living', 15, threshold_lower=0.80, threshold_upper=0.90, comparison_method='spacey')
         ]
 
     def __str__(self):
@@ -95,6 +153,16 @@ class Student:
                     first_student_attr.get_attr_comparison_score(
                         second_student_attr)
         return score
+    
+    def friendship_score_from_userdefined_attributes(self, second_student):
+        first_student = self
+        score = 0
+        if first_student.can_meet(second_student):
+            for (first_student_attr, second_student_attr) in zip(first_student.user_defined_attributes, second_student.user_defined_attributes):
+                score = score + \
+                    first_student_attr.get_attr_comparison_score(
+                        second_student_attr)
+        return score
 
     @classmethod
     def load_master_list(cls, filename):
@@ -124,11 +192,12 @@ class Student:
 
 
 def main():
+
     (header_list, student_list) = Student.load_master_list('master_list.csv')
-    print(student_list[39].friendship_score_from_predefined_attributes(
-        student_list[40]))
-    print(student_list[39])
-    print(student_list[40])
+    print(student_list[27].friendship_score_from_userdefined_attributes(
+        student_list[26]))
+    print(student_list[27])
+    print(student_list[26])
 
 
 if __name__ == "__main__":
